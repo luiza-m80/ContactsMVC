1dd93120b8eb1341774453ed8f13f2fb88acec6d
luiza-m80 <luuhbam@gmail.com>
2022-04-03 18:43:28 -0300

    Initial Commit

---
+++ ContactsMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsMVC
+//
+//  Created by Luiza Menezes Vieira de Souza on 03/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
